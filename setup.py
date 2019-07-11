0654aea16564aa4a0891118f806daa733da423d8
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2019-07-11 09:49:41 +0200

    initial project skeleton

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='ContainerCryptoUtils',
+    version='0.1',
+    description='Utilities to mount/unmount encrypted container files.',
+    packages=['schwarz.containercrypto'],
+    zip_safe=False,
+)
+
