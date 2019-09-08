0e65fc3c47d8ed33e4baae3fe343b59d35ec3cc3
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2019-09-08 10:58:49 +0200

    move project metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -2,22 +2,5 @@
 
 from setuptools import setup
 
-setup(
-    name='ContainerCryptoUtils',
-    version='0.1',
-    description='Utilities to mount/unmount encrypted container files.',
-    license='MIT',
-    packages=['schwarz.containercrypto'],
-    scripts=[
-        'scripts/crypted-container-ctl',
-    ],
-    install_requires=(
-        'docopt',
-    ),
-    tests_require=(
-        'PythonicTestcase',
-    ),
-    zip_safe=False,
-    python_requires='>=3.6',
-)
+setup()
 
