bf73ff0d30d5a963688e3c75080f00e1eb37edbe
Felix Schwarz <felix.schwarz@oss.schwarz.eu>
2019-11-03 10:10:03 +0100

    create-executable.sh: empty "build"/"dist" directories

--- create-executable.sh
+++ create-executable.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+for DIR_NAME in build dist; do
+    if [ -e "${DIR_NAME}" ]; then
+        trash "${DIR_NAME}"
+    fi
+done
+
 . venv/bin/activate
 # ensure all dependencies are installed
 python3 setup.py develop
 pyinstaller crypted-container-ctl.spec
+
+trash build
+
